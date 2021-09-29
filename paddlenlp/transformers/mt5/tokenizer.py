a39610618745302ebacdd1ae467feada10341970
liyulin <85541451+lylcst@users.noreply.github.com>
2021-09-30 00:01:42 +0800

    Update tokenizer.py

--- paddlenlp/transformers/mt5/tokenizer.py
+++ paddlenlp/transformers/mt5/tokenizer.py
@@ -2,5 +2,8 @@ import os
 import unicodedata
 from shutil import copyfile
 
-class MT5Tokenizer(PreTrainedTokenizer):
+from paddle.utils import try_import
+from .. import PretrainedTokenizer
+
+class MT5Tokenizer(PretrainedTokenizer):
   pass
