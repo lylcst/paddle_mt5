8d41c4c6f5f9c35a196c900bdac6e489ef76052d
liyulin <85541451+lylcst@users.noreply.github.com>
2021-10-01 20:49:17 +0800

    Update tokenizer.py

--- paddlenlp/transformers/mt5/tokenizer.py
+++ paddlenlp/transformers/mt5/tokenizer.py
@@ -18,23 +18,40 @@ class MT5Tokenizer(PretrainedTokenizer):
         "do_lower_case": True
     }
   }
-  def __init__(self,
-             vocab_file,
-             do_lower_case=True,
-             unk_token="[UNK]",
-             sep_token="[SEP]",
-             pad_token="[PAD]",
-             cls_token="[CLS]",
-             mask_token="[MASK]"):
+  def __init__(
+          self,
+          vocab_file,
+          sentencepiece_model_file,
+          eos_token="</s>",
+          unk_token="<unk>",
+          pad_token="<pad>",
+          extra_ids=100,
+          additional_special_tokens=None,
+          **kwargs
+      ):
+          # Add extra_ids to the special token list
+          if extra_ids > 0 and additional_special_tokens is None:
+              additional_special_tokens = [f"<extra_id_{i}>" for i in range(extra_ids)]
+          elif extra_ids > 0 and additional_special_tokens is not None:
+              # Check that we have the right number of extra_id special tokens
+              extra_tokens = len(set(filter(lambda x: bool("extra_id" in str(x)), additional_special_tokens)))
+              if extra_tokens != extra_ids:
+                  raise ValueError(
+                      f"Both extra_ids ({extra_ids}) and additional_special_tokens ({additional_special_tokens}) are provided to T5Tokenizer. "
+                      "In this case the additional_special_tokens must include the extra_ids tokens"
+                  )
 
-    if not os.path.isfile(vocab_file):
-        raise ValueError(
-            "Can't find a vocabulary file at path '{}'. To load the "
-            "vocabulary from a pretrained model please use "
-            "`tokenizer = BertTokenizer.from_pretrained(PRETRAINED_MODEL_NAME)`"
-            .format(vocab_file))
-    self.vocab = self.load_vocabulary(vocab_file, unk_token=unk_token)
-    self.basic_tokenizer = BasicTokenizer(do_lower_case=do_lower_case)
-    self.wordpiece_tokenizer = WordpieceTokenizer(
-        vocab=self.vocab, unk_token=unk_token)
-  
+          self.vocab_file = vocab_file
+          self._extra_ids = extra_ids
+          spm = try_import("sentencepiece")
+          self.sp_model = spm.SentencePieceProcessor()
+          self.sp_model.Load(sentencepiece_model_file)
+    @property
+    def vocab_size(self):
+        return len(self.sp_model)
+
+    def get_vocab(self):
+        vocab = {self.convert_ids_to_tokens(i): i for i in range(self.vocab_size)}
+        vocab.update(self.added_tokens_encoder)
+        return vocab
+
