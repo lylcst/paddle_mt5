477e3421ff881093f36598412b37f2ab53e19f9d
liyulin <85541451+lylcst@users.noreply.github.com>
2021-10-17 18:37:00 +0800

    Update tokenizer.py

--- paddlenlp/transformers/mt5/tokenizer.py
+++ paddlenlp/transformers/mt5/tokenizer.py
@@ -1,74 +1,71 @@
 import os
 import unicodedata
 from shutil import copyfile
+from typing import Any, Dict, List, Optional, Tuple
 
 from paddle.utils import try_import
 from .. import PretrainedTokenizer
 
 
-__all__ = ['MT5Tokenizer']
+__all__ = ['T5Tokenizer']
 
-class MT5Tokenizer(PretrainedTokenizer):
-    resource_files_names = {"vocab_file": "vocab.txt"}
-    pretrained_resource_files_map = {
-        "vocab_file": {
-            "t5-small": "https://huggingface.co/t5-small/resolve/main/spiece.model",
-            "t5-base": "https://huggingface.co/t5-base/resolve/main/spiece.model",
-            "t5-large": "https://huggingface.co/t5-large/resolve/main/spiece.model",
-            "t5-3b": "https://huggingface.co/t5-3b/resolve/main/spiece.model",
-            "t5-11b": "https://huggingface.co/t5-11b/resolve/main/spiece.model",
-        }
+
+class T5Tokenizer(PretrainedTokenizer):
+
+    resource_files_names = {
+        "sentencepiece_model_file": "spiece.model",
+        "vocab_file": "vocab.txt",
     }
 
     def __init__(
-          self,
-          vocab_file,
-          sentencepiece_model_file,
-          eos_token="</s>",
-          unk_token="<unk>",
-          pad_token="<pad>",
-          extra_ids=100,
-          additional_special_tokens=None,
-          **kwargs
-      ):
-          # Add extra_ids to the special token list
-          if extra_ids > 0 and additional_special_tokens is None:
-              additional_special_tokens = [f"<extra_id_{i}>" for i in range(extra_ids)]
-          elif extra_ids > 0 and additional_special_tokens is not None:
-              # Check that we have the right number of extra_id special tokens
-              extra_tokens = len(set(filter(lambda x: bool("extra_id" in str(x)), additional_special_tokens)))
-              if extra_tokens != extra_ids:
-                  raise ValueError(
-                      f"Both extra_ids ({extra_ids}) and additional_special_tokens ({additional_special_tokens}) are provided to T5Tokenizer. "
-                      "In this case the additional_special_tokens must include the extra_ids tokens"
-                  )
+        self,
+        vocab_file,
+        eos_token="</s>",
+        unk_token="<unk>",
+        pad_token="<pad>",
+        extra_ids=100,
+        additional_special_tokens=None,
+        sp_model_kwargs=None,
+        **kwargs
+    ):
+        if extra_ids > 0 and additional_special_tokens is None:
+            additional_special_tokens = [f"<extra_id_{i}>" for i in range(extra_ids)]
+        elif extra_ids > 0 and additional_special_tokens is not None:
+            # Check that we have the right number of extra_id special tokens
+            extra_tokens = len(set(filter(lambda x: bool("extra_id" in str(x)), additional_special_tokens)))
+            if extra_tokens != extra_ids:
+                raise ValueError(
+                    f"Both extra_ids ({extra_ids}) and additional_special_tokens ({additional_special_tokens}) are provided to T5Tokenizer. "
+                    "In this case the additional_special_tokens must include the extra_ids tokens"
+                )
 
-          self.vocab_file = vocab_file
-          self._extra_ids = extra_ids
-          spm = try_import("sentencepiece")
-          self.sp_model = spm.SentencePieceProcessor()
-          self.sp_model.Load(sentencepiece_model_file)
+        self.sp_model_kwargs = {} if sp_model_kwargs is None else sp_model_kwargs
+        self.vocab_file = vocab_file
+        self._extra_ids = extra_ids
+        self._extra_ids = extra_ids
+        spm = try_import("sentencepiece")
+        self.sp_model = spm.SentencePieceProcessor()
+        self.sp_model.Load(vocab_file)
+
     @property
     def vocab_size(self):
-        return len(self.sp_model)
+        return self.sp_model.get_piece_size() + self._extra_ids
 
     def get_vocab(self):
         vocab = {self.convert_ids_to_tokens(i): i for i in range(self.vocab_size)}
-        vocab.update(self.added_tokens_encoder)
+        # vocab.update(self.added_tokens_encoder)
         return vocab
 
     def get_special_tokens_mask(
-        self, token_ids_0, token_ids_1=None, already_has_special_tokens=False
-    ) :
+        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None, already_has_special_tokens: bool = False
+    ) -> List[int]:
+
         if already_has_special_tokens:
-            if token_ids_1 is not None:
-                raise ValueError(
-                    "You should not supply a second sequence if the provided sequence of "
-                    "ids id already formatted with special tokens for the model."
-                )
-            return super().get_special_tokens_mask(token_ids_0=token_ids_0,
-                                                   token_ids_1=token_ids_1,
-                                                   already_has_special_tokens=already_has_special_tokens)
+            return super().get_special_tokens_mask(
+                token_ids_0=token_ids_0, token_ids_1=token_ids_1, already_has_special_tokens=True
+            )
+
+        # normal case: some special tokens
         if token_ids_1 is None:
             return ([0] * len(token_ids_0)) + [1]
         return ([0] * len(token_ids_0)) + [1] + ([0] * len(token_ids_1)) + [1]
@@ -84,16 +81,19 @@ class MT5Tokenizer(PretrainedTokenizer):
             return token_ids + [self.eos_token_id]
 
     def create_token_type_ids_from_sequences(
-        self, token_ids_0, token_ids_1=None
-    ):
+        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
+    ) -> List[int]:
+
         eos = [self.eos_token_id]
+
         if token_ids_1 is None:
             return len(token_ids_0 + eos) * [0]
         return len(token_ids_0 + eos + token_ids_1 + eos) * [0]
 
     def build_inputs_with_special_tokens(
-        self, token_ids_0, token_ids_1=None
-    ):
+        self, token_ids_0: List[int], token_ids_1: Optional[List[int]] = None
+    ) -> List[int]:
+
         token_ids_0 = self._add_eos_if_not_present(token_ids_0)
         if token_ids_1 is None:
             return token_ids_0
@@ -116,7 +116,8 @@ class MT5Tokenizer(PretrainedTokenizer):
         self.sp_model = spm.SentencePieceProcessor(**self.sp_model_kwargs)
         self.sp_model.Load(self.vocab_file)
 
-    def _tokenize(self, text: str):
+    def _tokenize(self, text: str) -> List[str]:
+        """Take as input a string and return a list of strings (tokens) for words/sub-words"""
         return self.sp_model.encode(text, out_type=str)
 
     def _convert_token_to_id(self, token):
@@ -149,7 +150,24 @@ class MT5Tokenizer(PretrainedTokenizer):
         out_string += self.sp_model.decode_pieces(current_sub_tokens)
         return out_string.strip()
 
-    def save_vocabulary(self, save_directory, filename_prefix=None):
+    def convert_tokens_to_ids(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self._convert_token_to_id(tokens)
+        else:
+            return [self._convert_token_to_id(token) for token in tokens]
+
+    def convert_ids_to_tokens(self, ids, skip_special_tokens=False):
+        if not isinstance(ids, (list, tuple)):
+            return self._convert_id_to_token(ids)
+        tokens = [self._convert_id_to_token(_id) for _id in ids]
+        if skip_special_tokens:
+            return [
+                token for token in tokens
+                if token not in self.all_special_tokens
+            ]
+        return tokens
+
+    def save_vocabulary(self, save_directory: str, filename_prefix: Optional[str] = None) -> Tuple[str]:
         if not os.path.isdir(save_directory):
             logger.error(f"Vocabulary path ({save_directory}) should be a directory")
             return
